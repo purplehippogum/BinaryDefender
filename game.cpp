0f1683f436828553eeeeeddb597d0c8d088d2cbd
ZachVP <vegaperk@usc.edu>
2013-05-11 21:43:01 -0700

    Added GNU General Public License to the game repo.

--- game.cpp
+++ game.cpp
@@ -2,6 +2,25 @@
 #include "mainwindow.h"
 
 /** @mainpage
+* Copyright 2013 Zach Vega-Perkins
+* This program is distributed under the terms of the GNU General Public License
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/**
 * Main simply creates a new Qt application and runs it until
 * the player manually quits. Otherwise, the program will
 * restart
